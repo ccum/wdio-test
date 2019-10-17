9bdaa0bb8034b0190f1e9eed790be924c2ed82b6
Cesar Cueva <cueva1487@bbva.com>
2019-10-16 19:01:02 -0500

    test

--- features/step-definitions/check-title-step.js
+++ features/step-definitions/check-title-step.js
@@ -1,14 +1,21 @@
 // stepdefinitions.js
  
-const assert = require('assert')
-const { Given, Then } = require('cucumber')
+// const assert = require('assert')
+import { assert } from 'chai';
+import { Given,Then } from 'cucumber';
+
+//const { Given, Then } = require('cucumber')
+// import FromMascotas from '../../pageobjects/FromMascotas';
+//const FromMascotas = require("../../pageobjects/FromMascotas")
 
 Given('I go to the website', function () {
-    browser.url('https://www.google.com/');
+    browser.url('http://localhost:3000/');
 });
 
 Then('I expect the title of the page', function () {
-  browser.pause(10000);
-  assert.equal(browser.getTitle(), "Google")
+  
+  FromMascotas().putMascota();
+  assert.equal(browser.getTitle(), "React App");
+  browser.pause(1000);
  
 });
